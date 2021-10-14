eefd93d06bcee324c30dcc75bfa4540f986e314c
mati765 <mateusz.smuczynski@onet.pl>
2021-10-14 14:19:43 +0200

    siema

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject sqaureDropPrefab;
     public GameObject circleDropPrefab;
     public GameObject diamondDropPrefab;
+    public Text gameOverText;
+    public GameObject[] hearts;
+    public int playerHP = 3;
     float timeSinceLastDrop;
     // Start is called before the first frame update
     void Start()
@@ -42,5 +46,31 @@ public class GameManager : MonoBehaviour
     {
         float x = Random.Range(-10, 10);
         Instantiate(dropPrefab, new Vector2(x, 10), Quaternion.identity);
+    }
+    public void removeHeart()
+    {
+        playerHP--;
+        if (playerHP >= 3)
+        {
+            hearts[2].SetActive(true);
+        }
+        else hearts[2].SetActive(false);
+        if (playerHP >= 2)
+        {
+            hearts[1].SetActive(true);
+        }
+        else hearts[1].SetActive(false);
+        if (playerHP >= 1)
+        {
+            hearts[0].SetActive(true);
+        }
+        else hearts[0].SetActive(false);
+        if (playerHP <= 0)
+        {
+            Time.timeScale = 0;
+            gameOverText.gameObject.SetActive(true);
+        }
+
+
     }
 }
--- Assets/playerMove.cs
+++ Assets/playerMove.cs
@@ -6,12 +6,15 @@ public class playerMove : MonoBehaviour
 {
     public float jumpStrength = 400;
     public float moveSpeed = 5;
+    public GameObject gameManager;
+    public GameManager GM;
 
     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        GM = gameManager.GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -28,5 +31,11 @@ public class playerMove : MonoBehaviour
         }
         if (Input.GetAxisRaw("Horizontal") != 0)
             transform.position += (Vector3) Vector2.right * Input.GetAxisRaw("Horizontal") * Time.deltaTime * moveSpeed;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject drop = collision.gameObject;
+        Destroy(drop);
+        GM.removeHeart();
     }
 }
